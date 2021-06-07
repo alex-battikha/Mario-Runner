43bd84e9fb36d2d5432b2368701fd20874cf3691
alex-battikha <56514843+alex-battikha@users.noreply.github.com>
2021-06-06 23:09:34 -0700

    updated code; still debugging
    still debugging, WIP: game camera

--- sketch.js
+++ sketch.js
@@ -41,21 +41,25 @@ function setup() {
   backgroundSprite.scale = 1.6;
   backgroundSprite.addImage(backgroundImage);
 
+  background.velocityX = -3.5;
+
   player = createSprite(105, 300, 20, 20);
   player.scale = 0.3;
   player.addImage(marioImage);
 
-  heart1 = createSprite(50, 50);
-  heart1.scale = 0.05;
-  heart1.addImage(heartImage);
+  player.velocityX = 3.5;
 
-  heart2 = createSprite(100, 50);
-  heart2.scale = 0.05;
-  heart2.addImage(heartImage);
+  // heart1 = createSprite(50, 50);
+  // heart1.scale = 0.05;
+  // heart1.addImage(heartImage);
+
+  // heart2 = createSprite(100, 50);
+  // heart2.scale = 0.05;
+  // heart2.addImage(heartImage);
   
-  heart3 = createSprite(150, 50);
-  heart3.scale = 0.05;
-  heart3.addImage(heartImage);
+  // heart3 = createSprite(150, 50);
+  // heart3.scale = 0.05;
+  // heart3.addImage(heartImage);
 
   //For testing usage:
   // piranhaPlantLow = createSprite(width/2, height/2, width, height);
@@ -87,12 +91,14 @@ function setup() {
 function draw() {
   background(255);
 
-  playerLife();
-
-  //console.log(camera.position.x);
+  console.log(camera.position.x);
   //console.log(tubePosition);
 
-  if(gameState == 0) {    
+  if(gameState == 0) {
+    //Player Movement    
+
+    //player.velocityX += (score/10);
+
     //if-else conditions to move mario up and down
     if(keyDown(DOWN_ARROW) && player.y <= height-110) {
       player.y+=10;
@@ -112,8 +118,16 @@ function draw() {
     //   }
     // }
     
-    if(camera.position.x < width/2) {
-      camera.position.x = backgroundSPrite.width/2;
+    // if(camera.position.x < width/2) {
+    //   camera.position.x = backgroundSprite.width/2;
+    // }
+
+    // if(backgroundSprite.x < camera.position.x/1.5) {
+      // backgroundSprite.x = camera.position.x;
+    // }
+
+    if(backgroundSprite.x < camera.position.x) {
+      backgroundSprite.x = backgroundSprite.width/2
     }
 
     //Spawn coins and plants
@@ -122,17 +136,12 @@ function draw() {
     spawnPiranhas();
     //spawnPiranhasHigh();
 
-    //Check collision between mario and enemy and deduct heart
-    
-    //Increase score with detection of coin
-
     drawSprites();
+
+    playerLife();
   }
   else if(gameState == 1) {
     player.visible = false;
-
-    camera.position.x = camera.position.x;
-    player.x = player.x;
 
     count = 0;
 
@@ -151,11 +160,13 @@ function draw() {
     }
   }
 
+  camera.position.x = player.x + 320;
+
   //display score - text
   textSize(28);
   fill(0);
   textFont(marioFont);
-  text("Score" + " : " + score, 600+count, 65);
+  text("Score" + " : " + score, camera.position.x + 210, 65);
 }
 
 function playerLife() {
@@ -172,77 +183,84 @@ function playerLife() {
     hearts-=1;
   }
 
-  if(hearts == 3) {
-    heart1.visible = true;
-    heart2.visible = true;
-    heart3.visible = true;
-  } 
-
-  if(hearts == 2) {
-    heart1.visible = true;
-    heart2.visible = true;
-    heart3.visible = false;
-  }
-
-  if(hearts == 1) {
-    heart1.visible = true;
-    heart2.visible = false;
-    heart3.visible = false;
+  for(var i = 0; i < hearts; i++) {
+    image(heartImage, (60*i)+camera.position.x-370, 40, 55, 55);
   }
 
   if(hearts == 0) {
-    heart1.visible = false;
-    heart2.visible = false;
-    heart3.visible = false;
-
     gameState = 1;
   }
 
-  switch(score) {
-    case 0: camera.position.x+=4, 
-    player.x+=4, 
-    heart1.x+=4, 
-    heart2.x+=4,
-    heart3.x+=4,
-    count+=4;
-      break;
-    case 5: camera.position.x+=5,
-    player.x+=5,
-    heart1.x+=5,
-    heart2.x+=5,
-    heart3.x+=5,
-    count+=5;
-      break;
-    case 10: camera.position.x+=6,
-    player.x+=6,
-    heart1.x+=6,
-    heart2.x+=6,
-    heart3.x+=6,
-    count+=6;
-      break;
-    case 15: camera.position.x+=7,
-    player.x+=7,
-    heart1.x+=7,
-    heart2.x+=7,
-    heart3.x+=7,
-    count+=7;
-      break
-    case 20: camera.position.x+=8,
-    player.x+=8,
-    heart1.x+=8,
-    heart2.x+=8,
-    heart3.x+=8,
-    count+=8;
-      break;
-    case 25: camera.position.x+=9,
-    player.x+=9,
-    heart1.x+=9,
-    heart2.x+=9,
-    heart3.x+=9,
-    count+=9;
-      break;
-     default: break;
-  }
+  // if(hearts == 3) {
+  //   heart1.visible = true;
+  //   heart2.visible = true;
+  //   heart3.visible = true;
+  // } 
+
+  // if(hearts == 2) {
+  //   heart1.visible = true;
+  //   heart2.visible = true;
+  //   heart3.visible = false;
+  // }
+
+  // if(hearts == 1) {
+  //   heart1.visible = true;
+  //   heart2.visible = false;
+  //   heart3.visible = false;
+  // }
+
+  // if(hearts == 0) {
+    // heart1.visible = false;
+    // heart2.visible = false;
+    // heart3.visible = false;
+    // gameState = 1;
+  // }
+
+  // switch(score) {
+  //   case 0: camera.position.x+=4, 
+  //   player.x+=4, 
+  //   heart1.x+=4, 
+  //   heart2.x+=4,
+  //   heart3.x+=4,
+  //   count+=4;
+  //     break;
+  //   case 5: camera.position.x+=5,
+  //   player.x+=5,
+  //   heart1.x+=5,
+  //   heart2.x+=5,
+  //   heart3.x+=5,
+  //   count+=5;
+  //     break;
+  //   case 10: camera.position.x+=6,
+  //   player.x+=6,
+  //   heart1.x+=6,
+  //   heart2.x+=6,
+  //   heart3.x+=6,
+  //   count+=6;
+  //     break;
+  //   case 15: camera.position.x+=7,
+  //   player.x+=7,
+  //   heart1.x+=7,
+  //   heart2.x+=7,
+  //   heart3.x+=7,
+  //   count+=7;
+  //     break
+  //   case 20: camera.position.x+=8,
+  //   player.x+=8,
+  //   heart1.x+=8,
+  //   heart2.x+=8,
+  //   heart3.x+=8,
+  //   count+=8;
+  //     break;
+  //   case 25: camera.position.x+=9,
+  //   player.x+=9,
+  //   heart1.x+=9,
+  //   heart2.x+=9,
+  //   heart3.x+=9,
+  //   count+=9;
+  //     break;
+  //    default: break;
+  // }
 }
 
 function spawnCoins() {
@@ -376,6 +394,7 @@ function spawnPiranhas() {
     }
   }
 }
+
 function spawnPiranhasHigh() {
   if(frameCount % 190 == 0) {
     piranhaPlantHigh = createSprite(camera.position.x + 470, camera.position+70);
